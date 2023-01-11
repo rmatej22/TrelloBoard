63ee619a5b4badd3298f6b3224fe954bcaffe0e0
rmatej22 <rmatej545@gmail.com>
2023-01-11 16:21:23 +0100

    change package version & import package modules to angular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { WorkspaceComponent } from './components/inside/workspace/workspace.component';
 import { BoardComponent } from './components/inside/board/board.component';
+import { GravatarModule } from 'ngx-gravatar';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     WorkspaceComponent,
-    BoardComponent
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    GravatarModule,
+    NgxSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
