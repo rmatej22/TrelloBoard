85f714e748161a47890f718d8cece5dd09e28d68
rmatej22 <rmatej545@gmail.com>
2023-01-14 00:52:22 +0100

    add guard to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BoardComponent } from './components/inside/board/board.component';
 import { WorkspaceComponent } from './components/inside/workspace/workspace.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,10 +13,12 @@ const routes: Routes = [
   {
     path: 'workspace',
     component: WorkspaceComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'workspace/:id',
     component: BoardComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
