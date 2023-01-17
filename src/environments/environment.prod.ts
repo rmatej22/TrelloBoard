a2f15b3f69b1161f305a2696e56f794de75a60ff
rmatej22 <rmatej545@gmail.com>
2023-01-17 18:28:59 +0100

    add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://dzqethqqsvyffsoqniuz.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWV0aHFxc3Z5ZmZzb3FuaXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMyODc3NjYsImV4cCI6MTk4ODg2Mzc2Nn0.oUw-EU2DCI-HtoQrvH9EMBR-6EfSZwuDbupmiVCJ_oY',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://dzqethqqsvyffsoqniuz.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWV0aHFxc3Z5ZmZzb3FuaXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMyODc3NjYsImV4cCI6MTk4ODg2Mzc2Nn0.oUw-EU2DCI-HtoQrvH9EMBR-6EfSZwuDbupmiVCJ_oY',
 };
 
 /*
